8da3c11510ed4a11eb1dab33847626945ee598d7
Artur Semenas <uncelfuka@gmail.com>
2024-10-21 11:44:57 +0200

    shortlinks service init

---
+++ src/test/java/lt/arts/shortlinks/ShortlinksApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.arts.shortlinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlinksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
